7a40bc645e1d17d71c83b78faeac238bd51635af
Ashutosh Singh <singhashu598apr@gmail.com>
2021-06-15 14:49:42 +0530

    Portfolio First Commit

---
+++ myscript.js
@@ -0,0 +1,33 @@
+//-----------------------top responsive------------------------//
+
+var window_width = window.innerWidth;
+console.log(window_width);
+var check_Box1 = document.getElementById('side_nav1');
+check_Box1.addEventListener('click', function(){
+    console.log('clicked on side nav1');
+    document.getElementById("click1").checked = false;
+    });
+var check_Box2 = document.getElementById('side_nav2');
+check_Box2.addEventListener('click', function(){
+    console.log('clicked on side nav2');
+    document.getElementById("click1").checked = false;
+    });
+//var check_Box3 = document.getElementById('side_nav3');
+//check_Box3.addEventListener('click', function(){
+//    console.log('clicked on side nav3');
+//  });
+var check_Box4 = document.getElementById('side_nav4');
+check_Box4.addEventListener('click', function(){
+    console.log('clicked on side nav4');
+    document.getElementById("click1").checked = false;
+    });
+var check_Box5 = document.getElementById('side_nav5');
+check_Box5.addEventListener('click', function(){
+    console.log('clicked on side nav5');
+    document.getElementById("click1").checked = false;
+    });
+var check_Box6 = document.getElementById('side_nav6');
+check_Box6.addEventListener('click', function(){
+    console.log('clicked on side nav6');
+    document.getElementById("click1").checked = false;
+    });
